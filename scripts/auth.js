0e07cca900544f7492f3371c3c4a67ce4b69f6c9
enfenry <enfenry@vt.edu>
2019-10-30 10:36:00 -0400

    added Google Sign-in

--- scripts/auth.js
+++ scripts/auth.js
@@ -27,6 +27,42 @@ firebase.initializeApp(config);
 const auth = firebase.auth();
 var database = firebase.database();
 
+//  GOOGLE AUTH
+// ------------------------------------------------------
+
+var provider = new firebase.auth.GoogleAuthProvider();
+console.log(provider);
+
+$('#login-google').on('click', function (event) {
+    event.preventDefault();
+
+    auth.signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+        console.log('token',token,'user',user)
+
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+
+        console.log(errorCode,errorMessage,email,credential)
+      });
+
+});
+
+
+
+// ------------------------------------------------------
+
 // DECLARE A VARIABLE TO STORE CURRENT USER'S ID
 var currentUID = "";
 
